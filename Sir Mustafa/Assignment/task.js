120099f8eab1d098914c477ba7deae014fd3e1bc
Asghar Abbas <101616115+Abbasjafri-syed@users.noreply.github.com>
2022-04-19 09:31:11 +0100

    Update task.js
    Database Updating

--- Sir Mustafa/Assignment/task.js
+++ Sir Mustafa/Assignment/task.js
@@ -3,65 +3,51 @@ const app = express()
 const path = require("path");
 const bodyParser = require("body-parser")
 const PORT = 3000
-
-
-
-// File System
 const fs = require("fs")
-
-// DB Path
 const db = "./Public-db/data.json"
-
-// middlewares
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 
 app.get("/filter/:pass", (req, res) => {
     let data = JSON.parse(fs.readFileSync(db, "utf-8"))    
     pass = req.params.pass
-        
+ 
     fildata = data.filter(passwd => {
        if (passwd.pass == pass)
             return passwd.name
-        })
-        console.log(fildata);
-    
-    res.json(typeof fildata !== 'undefined' ? fildata : "User not found")
-    
-})
+ })
+            res.json(fildata)
 
-app.post('/del/:name', (req, res) => {
+          })
+
+  
+  app.get('/del/:name', (req, res) => {
     let data = JSON.parse(fs.readFileSync(db, "utf-8"))    
     const name = req.params.name;
-
-      if (data >= 1) {
-          let rcrd = data.delete(data.name)
-          res.send(rcrd)
-
-      }
-
-  })
-
-  let username= { 
-    "name":'Hasan',
-    "pass": 234 }
-  
-  app.post('/update/:users', (req, res) => {
-    let data = JSON.parse(fs.readFileSync(db, "utf-8"))    
-    const name = req.query.name;
+    remv = data.findIndex(x => x.name == name)
+    console.log(remv)
+    data.splice(remv, 1)
+  remdata = fs.writeFileSync(db, JSON.stringify(data))
+   res.send('User removed from Database')
+     })
     
-    
-      if (req.query !== username) {
-        console.log('User not Exist'); }
-        else{
-          data.push(username)
-          res.json('user Added')
-          
-        //   (Object.hasOwnProperty.call(name, "Hasan"))
-        //   res.json(typeof fildata !== 'undefined' ? fildata : "User not found")
-          
-      }
 
+
+  app.get('/update/:name', (req, res) => {
+    let data = JSON.parse(fs.readFileSync(db, "utf-8"))
+    named=req.params.name
+        username= data.find(users => {
+      users.name== named
+    })
+    if (typeof username !== 'undefined'){
+    res.send('User already Exist')
+  }
+    else{
+    newuser = {name: req.query.name , pass: req.query.pass}
+    data.push(newuser)
+    res.send('New User Added')
+}
+  fs.writeFileSync(db, JSON.stringify(data))
   })
 
 
